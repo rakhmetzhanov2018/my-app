1ca10f88f49a62798a82c732a93e66fec53ebfcd
rakhmetzhanov2018 <74981297+rakhmetzhanov2018@users.noreply.github.com>
2025-06-14 13:57:08 +0300

    final adjustments

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,13 +1,18 @@
 import { Input } from "antd";
 import FooterPicture from "../Images/FooterPicture.png"
+import { SendIcon, SocialMediaIcons } from "../Images";
+import { Navigate, useNavigate } from "react-router-dom";
 
 export function Footer() {
+    const navigate = useNavigate();
     return <footer>
         <div className="footer-div">
             <h1>Exclusive</h1>
             <h2>Subscribe</h2>
             <p>Get 10% off your first order</p>
-            <Input/>
+            <Input id="footer-input" style={{border: 'solid white 1px', backgroundColor: 'black', 
+            fontSize: '20px', color: 'white', width: '250px'}} 
+            placeholder="Enter your email" suffix={<img src={SendIcon}></img>}/>
         </div>
         <div className="footer-div">
             <h2>Support</h2>
@@ -18,22 +23,23 @@ export function Footer() {
         <div className="footer-div">
             <h2>Account</h2>
             <p>My Account</p>
-            <p>Login / Register</p>
+            <button className="footer-button" onClick={() => {navigate('/sign-up')}}>Login / Register</button>
             <p>Cart</p>
             <p>Wishlist</p>
-            <p>Shop</p>
+            <button className="footer-button" onClick={() => {navigate('/home#products')}}>Shop</button>
         </div>
         <div className="footer-div">
             <h2>Quick Link</h2>
             <p>Privacy Policy</p>
             <p>Terms Of Use</p>
             <p>FAQ</p>
-            <p>Contact</p>
+            <button className="footer-button" onClick={() => {navigate('/contact')}}>Contact</button>
         </div>
         <div className="footer-div">
             <h2>Download App</h2>
-            <div>Save $3 with App New User Only</div>
-            <img src={FooterPicture}></img>
+            <p>Save $3 with App New User Only</p>
+            <p><img src={FooterPicture}></img></p>
+            <img src={SocialMediaIcons}></img>
         </div>
     </footer>
 }
